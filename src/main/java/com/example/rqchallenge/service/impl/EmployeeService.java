b93ab1efccd31975ed75e5a5546aa1dfbc1331e3
Nikhil Apte <nikatpict@gmail.com>
2024-03-05 22:26:39 +0530

    Adding impl package for service

--- src/main/java/com/example/rqchallenge/service/impl/EmployeeService.java
+++ src/main/java/com/example/rqchallenge/service/impl/EmployeeService.java
@@ -1,10 +1,11 @@
-package com.example.rqchallenge.service;
+package com.example.rqchallenge.service.impl;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.rqchallenge.clients.dummy.DummyEmployeeApiClient;
 import com.example.rqchallenge.model.Employee;
+import com.example.rqchallenge.service.IEmployeeService;
 
 @Service
 public class EmployeeService implements IEmployeeService {
