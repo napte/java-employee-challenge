3c2001a97cba0d4a78d61eaa48e03aa29b38c6ec
Nikhil Apte <nikatpict@gmail.com>
2024-03-06 23:31:54 +0530

    Added Javadocs

--- src/main/java/com/example/rqchallenge/service/IEmployeeService.java
+++ src/main/java/com/example/rqchallenge/service/IEmployeeService.java
@@ -12,9 +12,26 @@ public interface IEmployeeService {
 
   void deleteEmployeeById(long id);
 
+  /**
+   * Searches all employees whose name contains or matches the specified input string.
+   * 
+   * @param searchString search string
+   * @return List of matching employees
+   */
   List<Employee> getEmployeesByName(String searchString);
 
+  /**
+   * Return the highest salary among all the employees
+   * 
+   * @return highest salary
+   */
   Integer getHighestSalary();
 
+  /**
+   * Return a list of the top <tt>N</tt> employees based on their salaries
+   * 
+   * @param count count of top records to fetch
+   * @return resultant top N employees
+   */
   List<Employee> getTopNBySalary(int count);
 }
